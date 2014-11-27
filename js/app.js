6a80085efec2588ccdb83f136bb94833eb56935e
Alex Crawford <awc737@gmail.com>
2014-11-26 21:00:23 -0800

    init

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 $(document).foundation();
+
+$(document).ready(function(){
+	$('#headslider').slick({
+		dots: true
+	});
+});
+
+/*
+$(function() {
+    var BV = new $.BigVideo({
+    	container: $('#headerbg')
+    });
+    BV.init();
+    BV.show('http://vjs.zencdn.net/v/oceans.mp4');
+});
+*/
